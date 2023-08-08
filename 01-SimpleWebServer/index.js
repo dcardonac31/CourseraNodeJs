4b7f5de9dbca9a2d89af274b1be1880851dcbd03
David Sneider Cardona Cardenas <dcardonac@outlook.com>
2023-08-07 22:28:13 -0500

    01-simple-web-server

---
+++ 01-SimpleWebServer/index.js
@@ -0,0 +1,27 @@
+var http = require('http');
+let server = http.createServer(function(request, response){
+    let body = "Hello world!!!";
+    let bodyJson = {
+        "Greeting": "Hello World!!!"
+    }
+
+    // Conver JSON object to string
+    let jsonString = JSON.stringify(bodyJson);
+
+    //Response text/plain
+    // response.writeHead(200, {
+    //     'Content-Length': body.length,
+    //     'Content-Type':'text/plain'
+    // });
+
+    // response.end(body);
+    
+    response.writeHead(200, {
+        'Content-Length': Buffer.byteLength(jsonString), // Calcular la longitud en bytes de la cadena JSON
+        'Content-Type': 'application/json' // Usar el tipo de contenido adecuado para JSON
+    });   
+
+    response.end(jsonString);
+});
+
+server.listen(8080);
